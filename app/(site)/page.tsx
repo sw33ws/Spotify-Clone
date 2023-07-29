d8bfa7059327abed9a478a2d518177bd2b410256
sw33ws <gillisl22@outlook.com>
2023-07-28 22:25:57 -0400

    middleware, and in progress

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,14 @@
+import getSongs from "@/actions/getSongs";
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
 
-export default function Home() {
+import PageContent from "./components/pageContent";
+
+export const revalidate = 0;
+
+export default async function Home() {
+  const songs = await getSongs();
+
   return (
     <div className="
     bg-neutral-900
@@ -45,9 +52,7 @@ export default function Home() {
             Newest Songs
           </h1>
         </div>
-        <div>
-          List of Songs!
-        </div>
+        <PageContent songs={songs} />
       </div>
     </div>
   )
